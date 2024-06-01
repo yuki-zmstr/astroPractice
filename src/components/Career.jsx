6e003106b23f76e07012036d4b488e6a9cb4c680
yuki-zmstr <yukihide99@gmail.com>
2024-06-01 08:26:44 +0800

    Use json to store text data

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,25 +1,17 @@
 import styles from './Career.module.css'
+import data from './data/careerData.json'
 
 export default function Career() {
     return (
         <div>
             <h4>Previous Education</h4>
             <ul>
-                <li>Shibuya Senior High School (2015-2018)</li>
-                <li>National University of Singapore (2020-2022)
-                    <ul>
-                        <li>
-                            B. Eng (Industrial Engineering), Mathematics minor</li>
-                    </ul>
-                </li>
+                {data.educationData.map(item => <li>{item}</li>)}
             </ul>
             <h4>Career</h4>
             <ul>
-                <li>(Full Time) Software Developer, Kotozna Ltd, Tokyo, Japan</li>
-                <li>(Internship) Data Analyst Intern, Abbott Laboratories, Singapore</li>
-                <li>(Internship) Operations Analyst Intern, Novelship Pte. Ltd, Singapore</li>
+                {data.workData.map(item => <li>{item}</li>)}
             </ul>
-
         </div>
     )
 }
