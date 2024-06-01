361c3c98e6c32aa359dd16c7d61f653b64cbaef3
yuki-zmstr <yukihide99@gmail.com>
2024-06-01 09:38:33 +0800

    Remove unnecessary keys

--- src/components/Learning.jsx
+++ src/components/Learning.jsx
@@ -1,16 +1,15 @@
 import data from './data/learningData.json'
 
 export default function Learning() {
-    const keys = Object.keys(data)
     return (
         <div>
             <h4>An archive of great learning material.</h4>
-            {keys.map(key => (
+            {data.entries.map(entry => (
                 <div>
-                    <h4>{data[key].header}</h4>
-                    {data[key].content.map(item => (
+                    <h4>{entry.header}</h4>
+                    {entry.content.map(item => (
                         <li>{item.title}&nbsp;
-                            <a href={item.link}>link</a>
+                            <a href={item.link} target="_blank">link</a>
                         </li>))}
                 </div>
             ))}
