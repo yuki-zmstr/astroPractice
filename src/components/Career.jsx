26ff9d587ead7a3b5ae6f216d1d555029c755251
yuki-zmstr <yukihide99@gmail.com>
2024-06-01 08:06:15 +0800

    Add content for Career

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,8 +1,25 @@
+import styles from './Career.module.css'
+
 export default function Career() {
     return (
         <div>
-            <p>Previous Education</p>
-            <p>B. Eng in Industrial Engineering, minor in Mathematics, National University of Singapore</p>
+            <h4>Previous Education</h4>
+            <ul>
+                <li>Shibuya Senior High School (2015-2018)</li>
+                <li>National University of Singapore (2020-2022)
+                    <ul>
+                        <li>
+                            B. Eng (Industrial Engineering), Mathematics minor</li>
+                    </ul>
+                </li>
+            </ul>
+            <h4>Career</h4>
+            <ul>
+                <li>(Full Time) Software Developer, Kotozna Ltd, Tokyo, Japan</li>
+                <li>(Internship) Data Analyst Intern, Abbott Laboratories, Singapore</li>
+                <li>(Internship) Operations Analyst Intern, Novelship Pte. Ltd, Singapore</li>
+            </ul>
+
         </div>
     )
 }
