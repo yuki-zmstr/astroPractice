0e2cde9140441045a1749d7da89305c08f9c440e
yuki-zmstr <yukihide99@gmail.com>
2024-06-01 09:00:58 +0800

    Basic content for Learning

--- src/components/Learning.jsx
+++ src/components/Learning.jsx
@@ -1,7 +1,19 @@
+import data from './data/learningData.json'
+
 export default function Learning() {
+    const keys = Object.keys(data)
     return (
         <div>
-            <p>Some stuff I found interesting</p>
+            <h4>An archive of great learning material.</h4>
+            {keys.map(key => (
+                <div>
+                    <h4>{data[key].header}</h4>
+                    {data[key].content.map(item => (
+                        <li>{item.title}&nbsp;
+                            <a href={item.link}>link</a>
+                        </li>))}
+                </div>
+            ))}
         </div>
     )
 }
