65ff7b97e007d4d69bc2736fa718027614a7d32e
yuki-zmstr <yukihide99@gmail.com>
2024-06-01 09:38:44 +0800

    Basic data for Philosophy

--- src/components/Philosophy.jsx
+++ src/components/Philosophy.jsx
@@ -1,7 +1,15 @@
+import data from './data/philosophyData.json'
+
 export default function Philosophy() {
     return (
         <div>
-            <p>Some stuff I want to say about life.</p>
+            <h4>Some stuff I want to say about life.</h4>
+            {data.entries.map(item => (
+                <div>
+                    <h4>{item.header}</h4>
+                    <p>{item.content}</p>
+                </div>
+            ))}
         </div>
     )
 }
